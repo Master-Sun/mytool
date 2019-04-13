9e7ea376770c7dd4e6af35cbaae59c1120db94ee
Master-Sun <47805388+Master-Sun@users.noreply.github.com>
2019-04-13 23:36:11 +0800

    Update verify.py

--- verify.py
+++ verify.py
@@ -11,26 +11,48 @@ def rndColor():
 
 
 def verifycode(request):
-    '''生成随机验证码，返回验证码图片并将随机字符保存进session'''
-    # 得到随机码
+    '''在django中用于生成随机验证码，返回验证码图片并将随机字符保存进session'''
+    # 得到4位随机码
     base_str = string.ascii_letters + string.digits
     rnd_list = random.sample(base_str,4)
     rnd_str = ''.join(rnd_list)
 
+    # 保存验证码到session
+    request.session['verifycode'] = rnd_str.lower()
+
     # 创建图片对象
-    bgcolor = 'gray'
+    bgcolor = 'lightgray'
     width,height = 80,25
     img = Image.new('RGB',(width,height),bgcolor)
 
+    # 关联字体文件，设置字体大小
+    font = ImageFont.truetype('/usr/share/fonts/truetype/fonts-japanese-gothic.ttf',20)
     # 创建画笔对象并书写验证码
     draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype('/usr/share/fonts/truetype/fonts-japanese-gothic.ttf',20)
     font_color = rndColor()
-    draw.text((15,2),rnd_str,font_color,font)
+    draw.text((15,2),rnd_str,fill=font_color,font=font)
 
+    # 随机生成100个散点
+    for i in range(100):
+        xy = (random.randint(0,width),random.randint(0,height))
+        draw.point(xy,fill=rndColor())
+
+    #　生成随机直线
+    for i in range(5):
+        x1 = random.randint(0,width)
+        y1 = random.randint(0,height)
+        x2 = random.randint(0,width)
+        y2 = random.randint(0,height)
+        draw.line(xy=(x1,y1,x2,y2),fill=rndColor(),width=2)
+
+    #　生成随机圆
+    for i in range(5):
+        x = random.randint(0,width)
+        y = random.randint(0,height)
+        draw.arc((x,y,x+5,y+5),0,360,fill=rndColor())
+        
     # 开辟二进制内存空间，存储图片
     buf = io.BytesIO()
     img.save(buf,'png')
-    request.session['verifycode'] = rnd_str.lower()
     
     return HttpResponse(buf.getvalue())
